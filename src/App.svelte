d038808ce532733ec35980fa03173810216b0cdd
Yoric Mangeart <yoricm@free.fr>
2022-05-23 22:26:14 +0200

    stats component added

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import FeedbackList from "./components/FeedbackList.svelte";
+  import FeedbackStats from "./components/FeedbackStats.svelte";
 
   let feedback = [
     {
@@ -18,9 +19,15 @@
       text: "Eiusmod occaecat irure eu quis ipsum qui in ullamco ipsum dolor amet. Aliqua irure est duis enim non voluptate tempor adipisicing anim esse commodo ipsum id. In ipsum irure qui non voluptate magna mollit sit. In ipsum ut esse cupidatat cillum labore voluptate adipisicing sunt.",
     },
   ];
+  let count;
+  let average;
+  $: count = feedback.length;
+  $: average =
+    feedback.reduce((prev, current) => prev + current.rating, 0) / count;
 </script>
 
 <main class="container">
+  <FeedbackStats {count} {average} />
   <FeedbackList
     {feedback}
     on:delete-feedback={(fb) =>
---
+++ src/components/FeedbackStats.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let count = 0;
+  export let average = 0;
+</script>
+
+<div class="feedbackStats">
+  <h4>{count} Reviews</h4>
+  <h4>Rating average: {Math.round(average * 100) / 100}</h4>
+</div>
+
+<style>
+  .feedbackStats {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
